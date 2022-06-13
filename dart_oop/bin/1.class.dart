f775c1bb86b9357a4c95edce0c1bad047340f0c3
ricky-blip <ricky.rinaldy77@gmail.com>
2022-06-13 20:03:43 +0700

    update

--- dart_oop/bin/1.class.dart
+++ dart_oop/bin/1.class.dart
@@ -1,7 +1,29 @@
-class Person {
+class Person {  // NOTE membuat class
   
+  // NOTE membuat Field
+  String name = "Guest";
+  String? address;
+  final String country = "Indonesia";
+
+
 }
 
-void main() {
+void main() { 
+
+  // NOTE membuat objek
+  var person1 = Person();
+
+  // NOTE manipulasi Field
+  person1.name = "Ricky Rinaldy";
+  person1.address = "Palembang";
+  // person1.country = null; TIDAK BISA DIUBAH KARENA FINAL field
+
+  print(person1.name); // NOTE mengambil data dari field 
+  print(person1.address);
+  print(person1.country);
+
+  Person person2 = Person();
+  print(person2);
+
 
 }
