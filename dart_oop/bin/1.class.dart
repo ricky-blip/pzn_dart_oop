c28a623a45d4bbadb7fc7e42cf7dbbbd234235a5
ricky-blip <ricky.rinaldy77@gmail.com>
2022-06-14 05:32:54 +0700

    update

--- dart_oop/bin/1.class.dart
+++ dart_oop/bin/1.class.dart
@@ -1,27 +1,38 @@
-class Person {  // NOTE membuat class
+class Person {  // NOTE 1. membuat class
   
-  // NOTE membuat Field
+  // NOTE 3. membuat Field
   String name = "Guest";
   String? address;
   final String country = "Indonesia";
 
+  // NOTE 6. membuat Method
+  void sayHello(String paramName) {
+    print("Hello ${paramName}, My name is ${name}");
+  }
+
+  String getName() {
+    return "HELLO Method Class Return, My name is $name";
+  }
 
 }
 
 void main() { 
 
-  // NOTE membuat objek
+  // NOTE 2. membuat objek
   var person1 = Person();
 
-  // NOTE manipulasi Field
+  // NOTE 5. manipulasi Field
   person1.name = "Ricky Rinaldy";
   person1.address = "Palembang";
   // person1.country = null; TIDAK BISA DIUBAH KARENA FINAL field
 
-  print(person1.name); // NOTE mengambil data dari field 
+  print(person1.name); // NOTE 4. mengambil data dari field 
   print(person1.address);
   print(person1.country);
+  person1.sayHello("Class"); // NOTE 7. memanggil class
+  print(person1.getName());
 
+  // NOTE 2. membuat objek
   Person person2 = Person();
   print(person2);
 
