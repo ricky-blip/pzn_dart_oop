cc4aeb3be61da03f90a23d715e6a1f1edf7a5b87
ricky-blip <ricky.rinaldy77@gmail.com>
2022-06-18 10:07:50 +0700

    update

---
+++ dart_oop/bin/10.redirecting_constuctor.dart
@@ -0,0 +1,40 @@
+class Kendaraan {
+
+  String name = "Guest";
+  String? brand;
+  final String country = "Germany";
+
+  // Constructor DEFAULT dengan formal parameter
+  Kendaraan(this.name, this.brand);
+
+  // Named Constructor dengan redirecting dari DEFAULT Constructor
+  Kendaraan.withName(String name) : this(name,"no Brand");
+
+  Kendaraan.withBrand(String brand) : this("no Name", brand);
+
+  // redirecting Constructor dari NamedConstructor
+  Kendaraan.fromIndonesia() : this.withBrand("No Brand Indonesia");
+
+  Kendaraan.noName() :this.withName("No Name");
+}
+
+void main() {
+  var k1 = Kendaraan("SUV", "VW");
+
+  print("${k1.name}, ${k1.brand} from ${k1.country}");
+
+  //objek Named Constructor & cara memanggilnya
+  var k2 = Kendaraan.withName("Vario");
+  print("${k2.name}, ${k2.brand} from ${k2.country}");
+
+  var k3 = Kendaraan.withBrand("Honda");
+  print("${k3.name}, ${k3.brand} from ${k3.country}");
+  
+  var k4 = Kendaraan.fromIndonesia();
+  print("${k4.name}, ${k4.brand} from ${k4.country}");
+  
+  var k5 = Kendaraan.noName();
+  print("${k5.name}, ${k5.brand} from ${k5.country}");
+
+
+}
---
+++ dart_oop/bin/9.named_constructor.dart
@@ -0,0 +1,31 @@
+class Kendaraan {
+
+  String name = "Guest";
+  String? brand;
+  final String country = "Germany";
+
+  // Constructor dengan formal parameter
+  Kendaraan(this.name, this.brand);
+
+  // Named Constructor agar bisa membuat lebih banyak constructor
+  Kendaraan.withName(this.name) {
+    
+  }
+
+  Kendaraan.withBrand(this.brand) {
+
+  }
+}
+
+void main() {
+  var mobil = Kendaraan("SUV", "VW");
+
+  print("${mobil.name}, ${mobil.brand} from ${mobil.country}");
+
+  //objek Named Constructor & cara memanggilnya
+  var rodaDua = Kendaraan.withName("Vario");
+  print("${rodaDua.name}, ${rodaDua.brand} from ${rodaDua.country}");
+
+  var rodaEmpat = Kendaraan.withBrand("Honda");
+  print("${rodaEmpat.name}, ${rodaEmpat.brand} from ${rodaEmpat.country}");
+}
