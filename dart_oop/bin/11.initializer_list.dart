56c67fa28ffd70cef0a28d5753b319da143bf364
ricky-blip <ricky.rinaldy77@gmail.com>
2022-06-19 11:07:39 +0700

    update

---
+++ dart_oop/bin/11.initializer_list.dart
@@ -0,0 +1,26 @@
+class Customer {
+  
+  String firstName = "";
+  String middleName = "";
+  String lastName = "";
+  String fullName = "";
+
+  Customer (this.fullName) 
+    // Membuat initializer List
+    : firstName = fullName.split(" ") [0], 
+      middleName = fullName.split(" ") [1], 
+      lastName = fullName.split(" ") [2] {
+
+    print("Create New Customer");
+
+  }
+}
+
+void main() {
+  var pelanggan = Customer("Ricky R Rinaldy");
+
+  print("Full Name : ${pelanggan.fullName}");
+  print(pelanggan.firstName);
+  print(pelanggan.middleName);
+  print(pelanggan.lastName);
+}
