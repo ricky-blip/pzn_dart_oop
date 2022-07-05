57f68279aa39b7c7f1dc1375076817db03a9ab43
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-05 22:12:27 +0700

    u

--- dart_oop/bin/37.typedef_function.dart
+++ dart_oop/bin/37.typedef_function.dart
@@ -1,7 +1,7 @@
 typedef Filter = String Function(String); // membuat alias dari anonymous function
 
 void sayHello(String name, Filter filter) {
-  print("Hello ${filter(name)}");
+  print("Well Hello, ${filter(name)}, nice to meet you");
 }
 
 void main() {
